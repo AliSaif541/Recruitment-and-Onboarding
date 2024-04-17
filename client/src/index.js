1c176382be92fa72262eaf41cd555a72dc445d47
Ali Saif <muhammadalisaif2@gmail.com>
2024-04-17 19:56:54 +0500

    Deployment

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App';
+// import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+// if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
